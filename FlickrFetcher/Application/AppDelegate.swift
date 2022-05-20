2526d42fbb561bbb4e89bbdabcdb065d9f24a9aa
Mohsen Khosravinia <khosravinia.mohsen@gmail.com>
2022-05-20 22:33:33 +0430

    Add linter.

--- FlickrFetcher/Application/AppDelegate.swift
+++ FlickrFetcher/Application/AppDelegate.swift
@@ -11,17 +11,19 @@ import AlamofireNetworkActivityLogger
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         NetworkActivityLogger.shared.startLogging()
         return true
     }
 
     // MARK: UISceneSession Lifecycle
 
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+    func application(_ application: UIApplication,
+                     configurationForConnecting connectingSceneSession: UISceneSession,
+                     options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {}
 }
-
--- Podfile
+++ Podfile
@@ -9,10 +9,23 @@ target 'FlickrFetcher' do
   pod 'Alamofire', '~> 5.4.0'
   pod 'AlamofireNetworkActivityLogger', '~> 3.4'
   pod 'Kingfisher', '~> 7.0'
+  pod 'SwiftLint'
 
   target 'FlickrFetcherTests' do
     inherit! :search_paths
     # Pods for testing
   end
 
+end
+
+# ----------------------------------------------------------------
+# brute force deployment target on pods
+# ----------------------------------------------------------------
+
+post_install do |installer|
+  installer.pods_project.targets.each do |target|
+    target.build_configurations.each do |config|
+      config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '13.0'
+    end
+  end
 end
